98a564940a979b203cde9717bd7de77ec86f60a7
Bakhtawar <bains.bakhtawar2328@gmail.com>
2024-02-11 22:53:11 +0530

    Updates

---
+++ Contact.a5155828.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire77dd;(0,e.register)("248A0",function(r,a){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var s=e("ayMG0"),t=()=>(0,s.jsxs)("div",{className:"mt-6 mb-6",children:[(0,s.jsx)("h1",{children:"Contact Us - +0192819281"}),(0,s.jsx)("h2",{className:"mt-5",children:"Instagram - FOODVILLA"})]})});
+//# sourceMappingURL=Contact.a5155828.js.map
